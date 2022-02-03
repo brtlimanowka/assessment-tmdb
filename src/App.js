0d2429cf9f005e4897d09698e1bd10138e48e18c
Bartosz Limanówka <brtlimanowka@gmail.com>
2022-02-03 19:57:17 +0100

    Router setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Switch>
+          {/* <Route exact path='/' component={Popular} /> */}
+          {/* <Route path='/movie/:id' component={Details} /> */}
+          {/* <Route exact path='/movie' render={() => <Redirect to='/' />} /> */}
+          <Route path='*' render={() => <Redirect to='/' />} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
