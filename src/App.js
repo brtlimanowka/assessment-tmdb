40784d809c886e613dca51cb7e625c624931871a
Bartosz Limanówka <brtlimanowka@gmail.com>
2022-02-03 21:52:01 +0100

    Troubleshooting

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import Popular from './components/Popular/Popular';
+// import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+// import Popular from './components/Popular/Popular';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Switch>
           <Route exact path='/' component={Popular} />
-          {/* <Route path='/movie/:id' component={Details} /> */}
-          {/* <Route exact path='/movie' render={() => <Redirect to='/' />} /> */}
+          <Route path='/movie/:id' component={Details} />
+          <Route exact path='/movie' render={() => <Redirect to='/' />} />
           <Route path='*' render={() => <Redirect to='/' />} />
         </Switch>
-      </BrowserRouter>
+      </BrowserRouter> */}
     </div>
   );
 }
