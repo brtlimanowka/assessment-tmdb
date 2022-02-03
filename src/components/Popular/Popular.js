a9480fd3857a9c8c4391558d229fb562ca1dc8f1
Bartosz Limanówka <brtlimanowka@gmail.com>
2022-02-03 21:38:54 +0100

    Troubleshooting

--- src/components/Popular/Popular.js
+++ src/components/Popular/Popular.js
@@ -1,35 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import Spinner from '../ui/Spinner';
-import { API_URL, API_KEY } from '../../utils/properties';
+// import React, { useState, useEffect } from 'react';
+// import Spinner from '../ui/Spinner';
+// import { API_URL, API_KEY } from '../../utils/properties';
 
-const Popular = () => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [popular, setPopular] = useState(null);
-  const [error, setError] = useState(null);
+// const Popular = () => {
+//   const [isLoading, setIsLoading] = useState(false);
+//   const [popular, setPopular] = useState(null);
+//   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    setIsLoading(true);
-    fetch(`${API_URL}/movie/popular?api_key=${API_KEY}`)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error(response.statusText);
-        }
-      })
-      .then((data) => {
-        setPopular(data);
-      })
-      .catch((error) => setError(error.message))
-      .finally(() => setIsLoading(false));
-  }, []);
+//   useEffect(() => {
+//     setIsLoading(true);
+//     fetch(`${API_URL}/movie/popular?api_key=${API_KEY}`)
+//       .then((response) => {
+//         if (response.ok) {
+//           return response.json();
+//         } else {
+//           throw new Error(response.statusText);
+//         }
+//       })
+//       .then((data) => {
+//         setPopular(data);
+//       })
+//       .catch((error) => setError(error.message))
+//       .finally(() => setIsLoading(false));
+//   }, []);
 
-  return (
-    <div>
-      {isLoading && <Spinner />}
-      {!isLoading && <span>Ok</span>}
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       {isLoading && <Spinner />}
+//       {!isLoading && <span>Ok</span>}
+//     </div>
+//   );
+// };
 
-export default Popular;
+// export default Popular;
