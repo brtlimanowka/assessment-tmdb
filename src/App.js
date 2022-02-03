ed45308a67511dc07bbc72765f1c766149eddbe4
Bartosz Limanówka <brtlimanowka@gmail.com>
2022-02-03 20:57:52 +0100

    Drop Context

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import MoviesState from './store/movies/MoviesState';
 import Popular from './components/Popular/Popular';
 import './App.css';
 
 function App() {
   return (
-    <MoviesState>
-      <div className='App'>
-        <BrowserRouter>
-          <Switch>
-            <Route exact path='/' component={Popular} />
-            {/* <Route path='/movie/:id' component={Details} /> */}
-            {/* <Route exact path='/movie' render={() => <Redirect to='/' />} /> */}
-            <Route path='*' render={() => <Redirect to='/' />} />
-          </Switch>
-        </BrowserRouter>
-      </div>
-    </MoviesState>
+    <div className='App'>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Popular} />
+          {/* <Route path='/movie/:id' component={Details} /> */}
+          {/* <Route exact path='/movie' render={() => <Redirect to='/' />} /> */}
+          <Route path='*' render={() => <Redirect to='/' />} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
