969352ad28c3ad434968398feeb949c238d75114
Bartosz Limanówka <brtlimanowka@gmail.com>
2022-02-04 20:53:32 +0100

    Add general URL

--- src/utils/properties.js
+++ src/utils/properties.js
@@ -1,2 +1,3 @@
 export const API_URL = 'https://api.themoviedb.org/3',
-  API_KEY = 'e3c3cf46cc2a758b5ab9c57b19846dde';
+  API_KEY = 'e3c3cf46cc2a758b5ab9c57b19846dde',
+  WEB_URL = 'https://www.themoviedb.org/';
