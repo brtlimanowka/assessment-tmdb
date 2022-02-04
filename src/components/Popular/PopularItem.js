2b5fb48e4dd1cf821dee03ac4d272b92b1b9d285
Bartosz Limanówka <brtlimanowka@gmail.com>
2022-02-04 20:53:45 +0100

    Display poster and title

---
+++ src/components/Popular/PopularItem.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+import { WEB_URL } from '../../utils/properties';
+
+const IMAGE_URL = `${WEB_URL}t/p/w300_and_h450_bestv2/`;
+
+const Card = styled.div`
+  height: 664px;
+  width: 399px;
+  box-shadow: ${({ theme }) => theme.shadow};
+  display: flex;
+  flex-direction: column;
+  div.card__poster {
+    margin: 30px auto;
+    height: 426px;
+    width: 338px;
+    border-radius: 47px;
+  }
+  div.card__details {
+    margin: 0 31px 30px 31px;
+    h2 {
+      font-size: 32px;
+    }
+  }
+`;
+
+const PopularItem = ({ data }) => {
+  const backgroundURL = `url(${IMAGE_URL}/${data.poster_path})`;
+  const title =
+    data.title.length > 19 ? data.title.substring(0, 18) + '...' : data.title;
+
+  return (
+    <Card>
+      <div
+        className='card__poster'
+        style={{ backgroundImage: backgroundURL }}></div>
+      <div className='card__details'>
+        <h2>{title}</h2>
+      </div>
+    </Card>
+  );
+};
+
+export default PopularItem;
