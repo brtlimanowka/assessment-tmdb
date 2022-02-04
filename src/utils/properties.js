3f18dea3dae9a6c03cc042a5c22de223ae00a00d
Bartosz Limanówka <brtlimanowka@gmail.com>
2022-02-04 21:08:16 +0100

    Remove trailing slash

--- src/utils/properties.js
+++ src/utils/properties.js
@@ -1,3 +1,3 @@
 export const API_URL = 'https://api.themoviedb.org/3',
   API_KEY = 'e3c3cf46cc2a758b5ab9c57b19846dde',
-  WEB_URL = 'https://www.themoviedb.org/';
+  WEB_URL = 'https://www.themoviedb.org';
